de7d1e4e64eb4adffd63e818876dceaa18fe2563
desertcoqui <fdetres@my365.bellevue.edu>
2021-12-30 16:11:03 -0600

    added connect metamask feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useMemo, use } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  //The connectWallet hook thirdweb gives us
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address);
+
+  //If user does'nt connect their wallet if statement is triggered.
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Rotten Aguacate</h1>
+        <h2>Dao</h2>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet, Porfavor
+        </button>
+      </div>
+    );
+  }
+
+  //Once user connects wallet we return this statement
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Rotten Aguacate</h1>
+      <h2>DAO</h2>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+//This imports ThirdWeb SDK
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+//Chains that are supported. 4= Rinkeby
+const supportedChainIds = [4];
+
+//Here is the type of wallet I decide to support
+//We are using MetaMask which is an "injected wallet"
+
+const connectors = {
+  injected: {},
+};
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
